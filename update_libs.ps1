78cc6336504678eb0e9ab7039a0a9b9bdf912254
Corné Frijters <cornefrijters@hotmail.com>
2024-09-07 08:05:05 +0200

    Implemented resource management including assets, resource importers (sprites, shaders), serialization and hot reloading.

---
+++ update_libs.ps1
@@ -0,0 +1,34 @@
+﻿## script in powershell because that's what I'm most comfortable with~
+
+$fnalibsPath = "$PSScriptRoot/packages/fnalibs"
+
+#Remove-Item $fnalibsPath -Recurse
+
+Write-Output "📫 Installing latest binaries..."
+
+$zipFnalibs = "$fnalibsPath/fnalibs.tar.bz2"
+
+New-Item -ItemType Directory -Force -Path $fnalibsPath
+
+Invoke-WebRequest -Uri "https://fna.flibitijibibo.com/archive/fnalibs.tar.bz2" -OutFile $zipFnalibs
+
+tar -xvzf $zipFnalibs -C $fnalibsPath
+Remove-Item $zipFnalibs
+
+Write-Output "🪪 Renaming files..."
+
+## now, let's rename the binaries so linux and mac are happy with us.
+Rename-Item -Path "${fnalibsPath}/lib64/libFAudio.so.0" -NewName libFAudio.so
+Rename-Item -Path "${fnalibsPath}/lib64/libFNA3D.so.0" -NewName libFNA3D.so
+Rename-Item -Path "${fnalibsPath}/lib64/libSDL2-2.0.so.0" -NewName libSDL2.so
+
+Rename-Item -Path "${fnalibsPath}/libaarch64/libFAudio.so.0" -NewName libFAudio.so
+Rename-Item -Path "${fnalibsPath}/libaarch64/libFNA3D.so.0" -NewName libFNA3D.so
+Rename-Item -Path "${fnalibsPath}/libaarch64/libSDL2-2.0.so.0" -NewName libSDL2.so
+
+Rename-Item -Path "${fnalibsPath}/osx" -NewName osx-x64
+
+Rename-Item -Path "${fnalibsPath}/osx-x64/libFAudio.0.dylib" -NewName libFAudio.dylib
+Rename-Item -Path "${fnalibsPath}/osx-x64/libFNA3D.0.dylib" -NewName libFNA3D.dylib
+Rename-Item -Path "${fnalibsPath}/osx-x64/libSDL2-2.0.0.dylib" -NewName libSDL2.dylib
+Rename-Item -Path "${fnalibsPath}/osx-x64/libvulkan.1.dylib" -NewName libvulkan.dylib
